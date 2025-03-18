a4fcf2b71359c1889c55270e93d5dfdbe396dcf7
Kikun-Kuroswa <dewadedek18@gmail.com>
2025-03-18 17:29:07 +0700

    Changes and comments

--- starlight within 0/src/entity/Entity.java
+++ starlight within 0/src/entity/Entity.java
@@ -1,19 +1,122 @@
 package entity;
 
 public class Entity {
-    public int x, y;
-    public int speed;
-    public String namaMoster;
-    public int jenisMosnter;
-    public int physicalMosterDamgae;
-    public int physicalMonsterArmor;
-    public int magicalMonsterDamage;
-    public int magicalMonsterArmor;
-    public int monsterHp;
-    public int monsterDrop;
+    protected int x, y; // Entity Cordinates 
+    protected int speed;// Entity's speed
+    protected String namaMoster; 
+    protected int jenisMosnter;
+    protected int physicalMosterDamgae;
+    protected int physicalMonsterArmor;
+    protected int magicalMonsterDamage;
+    protected int magicalMonsterArmor;
+    protected int monsterHp;
+    protected int monsterDrop;
+
+    public Entity() {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.namaMoster = namaMoster;
+        this.jenisMosnter = jenisMosnter;
+        this.physicalMosterDamgae = physicalMosterDamgae;
+        this.physicalMonsterArmor = physicalMonsterArmor;
+        this.magicalMonsterDamage = magicalMonsterDamage;
+        this.magicalMonsterArmor = magicalMonsterArmor;
+        this.monsterHp = monsterHp;
+        this.monsterDrop = monsterDrop;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getNamaMoster() {
+        return namaMoster;
+    }
+
+    public void setNamaMoster(String namaMoster) {
+        this.namaMoster = namaMoster;
+    }
+
+    public int getJenisMosnter() {
+        return jenisMosnter;
+    }
+
+    public void setJenisMosnter(int jenisMosnter) {
+        this.jenisMosnter = jenisMosnter;
+    }
+
+    public int getPhysicalMosterDamgae() {
+        return physicalMosterDamgae;
+    }
+
+    public void setPhysicalMosterDamgae(int physicalMosterDamgae) {
+        this.physicalMosterDamgae = physicalMosterDamgae;
+    }
+
+    public int getPhysicalMonsterArmor() {
+        return physicalMonsterArmor;
+    }
+
+    public void setPhysicalMonsterArmor(int physicalMonsterArmor) {
+        this.physicalMonsterArmor = physicalMonsterArmor;
+    }
+
+    public int getMagicalMonsterDamage() {
+        return magicalMonsterDamage;
+    }
+
+    public void setMagicalMonsterDamage(int magicalMonsterDamage) {
+        this.magicalMonsterDamage = magicalMonsterDamage;
+    }
+
+    public int getMagicalMonsterArmor() {
+        return magicalMonsterArmor;
+    }
+
+    public void setMagicalMonsterArmor(int magicalMonsterArmor) {
+        this.magicalMonsterArmor = magicalMonsterArmor;
+    }
+
+    public int getMonsterHp() {
+        return monsterHp;
+    }
+
+    public void setMonsterHp(int monsterHp) {
+        this.monsterHp = monsterHp;
+    }
+
+    public int getMonsterDrop() {
+        return monsterDrop;
+    }
+
+    public void setMonsterDrop(int monsterDrop) {
+        this.monsterDrop = monsterDrop;
+    }
 
     
 
+    
 
 
 }
